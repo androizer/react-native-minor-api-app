03310241d3398934660a7145b61081fbad9a5810
Akki Mahajan <glamnation.akki@gmail.com>
2018-08-25 19:30:23 +0530

    global config of chatapp

---
+++ src/assets/components/FirebaseConfig.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB0s1cBycIsTlncHCrYabK6245eQRceqCg',
+  authDomain: 'chatapp-7194.firebaseapp.com',
+  databaseURL: 'https://chatapp-7194.firebaseio.com',
+  storageBucket: 'chatapp-7194.appspot.com',
+});
+
+export default firebase;
